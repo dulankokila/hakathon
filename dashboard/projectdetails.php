b1e1fead3235a6eb80d497dfc9f2457c325b4d1d
dulankokila <dulankokilabandara@gmail.com>
2023-10-05 23:58:40 +0530

    sdfds

--- dashboard/projectdetails.php
+++ dashboard/projectdetails.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Create Task</title>
+    <title>Project details</title>
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" />
 
@@ -15,21 +15,17 @@
 
     <?php
     include '../header.php';
-    require '../connection.php'
+    require "../connection.php"
         ?>
 
     <div class="container-fluid pt-1 pt-md-2 pt-lg-0 mt-5">
     <?php
-    $project_data = Database::search("SELECT * FROM `user_has_projects` 
-INNER JOIN `user` ON `user_has_projects`.`user_email` = `user`.`email`
-INNER JOIN `projects` ON `user_has_projects`.`projects_id` = `projects`.`id` 
-INNER JOIN `user_role` ON `user_has_projects`.`user_role_id` = `user_role`.`id` 
-WHERE `user`.`email` = 'nal@gmail.com' AND `user_role`.`role`='admin';");
+           
+         $rs =    Database::search("SELECT * FROM `projects` INNER JOIN `user_has_projects` ON user_has_projects.projects_id=projects.id
+            WHERE `id`='3'");
 
-    $project_data_row = $project_data->num_rows;
-
-    $project = $project_data->fetch_assoc();
-    ?>
+         $data =    $rs->fetch_assoc();
+            ?>
 
     <div class="container-fluid pt-5 pt-md-4 pt-lg-3">
 
@@ -38,17 +34,15 @@ WHERE `user`.`email` = 'nal@gmail.com' AND `user_role`.`role`='admin';");
             <div class="col-12 py-3 px-1 px-md-4">
 
             <?php
-            require "../connection.php";
-         $rs =    Database::search("SELECT * FROM `projects` INNER JOIN `user_has_projects` ON user_has_projects.projects_id=projects.id
+           
+         $rsa =    Database::search("SELECT * FROM `projects` INNER JOIN `user_has_projects` ON user_has_projects.projects_id=projects.id
             WHERE `id`='3'");
 
-         $data =    $rs->fetch_assoc();
+         $dataa =    $rsa->fetch_assoc();
             ?>
 
                 <h4 class="border-bottom border-dark fw-bold pb-2 pb-md-2"><?php echo $data["name"]?></h4>
-                <h4 class="border-bottom border-dark fw-bold pb-2 pb-md-2">
-                    <?php echo $project["name"] ?>
-                </h4>
+                
 
                 <div class="d-flex flex-column flex-md-row">
 
@@ -63,16 +57,8 @@ WHERE `user`.`email` = 'nal@gmail.com' AND `user_role`.`role`='admin';");
                             </div>
 
                            
-                            <p class="fw-bold mb-2">Project Name :
-                                <?php echo $project["name"] ?>
-                            </p>
 
-                            <div class="d-flex flex-column">
-                                <p class="fw-bold m-0">Description :</p>
-                                <p class="m-0">
-                                    <?php echo $project["description"] ?>
-                                </p>
-                            </div>
+                            
 
                             <div class="d-flex flex-column mt-2 mb-2">
                                 <p class="fw-bold m-0">Tasks :</p>
@@ -94,23 +80,11 @@ WHERE `user`.`email` = 'nal@gmail.com' AND `user_role`.`role`='admin';");
                     </div>
                     <div class="col-12 col-md-5">
                         <div class="d-flex flex-column align-items-start align-items-md-end">
-                            <p class="fw-bold mb-2 text-danger">Project DeadLine :<?php echo $data[""]?></p>
-                            <p class="mb-2r">Project Start Date : 2023-11-23</p>
+                            <p class="fw-bold mb-2 text-danger">Project DeadLine :<?php echo $data["close_date"]?></p>
+                            <p class="mb-2r">Project Start Date : <?php echo $data["start_date"]?></p>
                             <div class="d-flex flex-column align-items-start align-items-md-end">
                                 <p class="fw-bold mb-2">Technologies</p>
-                                <ul class="list-group list-group-flush bg-transparent text-start text-md-end">
-
-                                    <?php
-                                    for ($x = 0; $x < $project_data_row; $x++) {
-                                        ?>
-
-                                        <li class="list-group-item bg-transparent m-0 px-1"><i class="bi bi-dot"></i>
-                                            <?php echo $project["technology"] ?>
-                                        </li>
-                                        <?php
-                                    }
-                                    ?>
-                                </ul>
+                                <p>   <?php echo $data["technology"] ?></p>
                             </div>
                         </div>
                     </div>
@@ -118,11 +92,7 @@ WHERE `user`.`email` = 'nal@gmail.com' AND `user_role`.`role`='admin';");
                 </div>
 
                 <?php
-                $project_member = Database::search("SELECT * FROM `user_has_projects` 
-INNER JOIN `user` ON `user_has_projects`.`user_email` = `user`.`email`
-INNER JOIN `projects` ON `user_has_projects`.`projects_id` = `projects`.`id` 
-INNER JOIN `user_role` ON `user_has_projects`.`user_role_id` = `user_role`.`id` 
-WHERE `user`.`email` != 'nal@gmail.com' AND `user_role`.`role`!='admin';");
+                $project_member = Database::search("SELECT * FROM `user_has_projects` INNER JOIN `user_role` ON user_role.id=user_has_projects.user_role_id WHERE `projects_id`='3'");
 
                 $project_member_row = $project_member->num_rows;
 
@@ -139,7 +109,7 @@ WHERE `user`.`email` != 'nal@gmail.com' AND `user_role`.`role`!='admin';");
                                 <th scope="col">#</th>
                                 <th scope="col">Name</th>
                                 <th scope="col">Role</th>
-                                <th scope="col">Task</th>
+                                
                                 <th scope="col">Manage</th>
                             </tr>
                         </thead>
@@ -151,20 +121,20 @@ WHERE `user`.`email` != 'nal@gmail.com' AND `user_role`.`role`!='admin';");
                                 <tr>
                                     <th scope="row">1</th>
                                     <td>
-                                        <?php echo $project_member_data["firstname"] ?>
-                                        <?php echo $project_member_data["lastname"] ?>
+                                        <?php echo $project_member_data["user_email"] ?>
+                                       
                                     </td>
                                     <td>
                                         <?php echo $project_member_data["role"] ?>
                                     </td>
-                                    <td>Otto</td>
+                                    
 
                                     <td class="">
-                                        <a href="memberprofile.php?email=<?php echo $project_member_data["email"] ?>" class="project-view-btn bg-dark text-white"><i
+                                        <a href="memberprofile.php?email=<?php echo $project_member_data["user_email"] ?>" class="project-view-btn bg-dark text-white"><i
                                                 class="bi bi-wrench"></i></a>
-                                        <a href="removememberprocess.php?email=<?php echo $project_member_data["email"] ?>&project_id=<?php echo $project_member_data["project_id"] ?>" class="project-view-btn bg-danger text-white"><i
+                                        <a href="removememberprocess.php?email=<?php echo $project_member_data["user_email"] ?>&project_id=<?php echo $project_member_data["projects_id"] ?>" class="project-view-btn bg-danger text-white"><i
                                                 class="bi bi-ban"></i></a>
-                                        <a href="memberprofile.php?email=<?php echo $project_member_data["email"] ?>"
+                                        <a href="memberprofile.php?email=<?php echo $project_member_data["user_email"] ?>"
                                             class="project-view-btn bg-success text-white"><i
                                                 class="bi bi-eye-fill"></i></a>
                                     </td>
