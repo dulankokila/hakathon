13b489add948494bcfbe52d2bd80ff190d4f655c
dulankokila <dulankokilabandara@gmail.com>
2023-10-05 14:16:09 +0530

    dfsd

--- header.php
+++ header.php
@@ -4,7 +4,7 @@
     <title>Document</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
     <link rel="stylesheet" href="bootstrap.css" />
-    <link rel="stylesheet" href="style.css" />
+    <link rel="stylesheet" href="style2.css" />
 
 </head>
 
@@ -17,20 +17,18 @@
                 <div class="col-5 col-lg-1 mt-1 text-white fw-bold" style="margin-left: 20px;"><img src="images/black.jpg" class="logo" /> SALFORD</div>
                 <a class="navbar-brand"></a>
                 <div class="mx-5"><i class="bi bi-person-circle text-white mx-5"></i>
-                    <i class="bi bi-chat-left text-white"></i>
-                </div>
-
+                <i class="bi bi-chat-left text-white"></i></div>
+                
                 <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasDarkNavbar" aria-controls="offcanvasDarkNavbar">
                     <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="offcanvas offcanvas-start text-bg-dark" tabindex="-1" id="offcanvasDarkNavbar" aria-labelledby="offcanvasDarkNavbarLabel">
                     <div class="offcanvas-header">
                         <button type="button" class="btn-close bg-white" data-bs-dismiss="offcanvas" aria-label="Close" style="margin-left: 350px;"></button>
-
-                    </div>
-                    <h4><img src="images/black.jpg" class="logo mx-1" /> SALFORD Management System</h4>
+                      
+                    </div>   <h4><img src="images/black.jpg" class="logo mx-1" /> SALFORD Management System</h4>
                     <div class="offcanvas-body">
-
+                       
                         <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
 
                             <li class="nav-item">
--- homenew.php
+++ homenew.php
@@ -5,16 +5,19 @@
     <title>HOME</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous" />
-    <link rel="stylesheet" href="home01/hstyle.css" />
-    <link rel="icon" href="home01/image/black.png" />
+   
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" />
+
+<link rel="stylesheet" href="../css/style.css">
+    <link rel="icon" href="image/black.png" />
 
 </head>
 
 <body class="body">
+<?php include "header.php" ?>
     <div class="container-fluid">
         <div class="row">
-        <?php include '../header.php' ?>
+       
 
             <div class="col-12">
                 <h1 class="mt-5 titel01">HOME</h1>
@@ -27,7 +30,7 @@
                             <label class="mt-2 fw-bold">
                                 What is SALFORD ?
                             </label>
-                            <img src="image/logo1.png">
+                            <img src="images/logo.png">
                         </div>
                     </div>
                     <div class="col-5 border border-3 b1">
@@ -44,6 +47,8 @@
         </div>
     </div>
     </div>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.3/dist/umd/popper.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
