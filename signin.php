1c66611b80979786115d7efcf608b6c19ae84aa1
dulankokila <dulankokilabandara@gmail.com>
2023-10-05 12:22:52 +0530

    Update signin.php

--- signin.php
+++ signin.php
@@ -51,6 +51,7 @@ if (isset($_COOKIE["password"])) {
           </div>
 
           <button  class="btn btn-primary btn-block mt-2" onclick="signin();">Sign In</button>
+          
 
           <p class="mt-3 text-center">Don't have an account? <a href="signup.php" class="signup-link">Sign Up</a></p>
 
@@ -59,9 +60,26 @@ if (isset($_COOKIE["password"])) {
               src="images/4202105_microsoft_logo_social_social media_icon.png" class="icon mx-3" /><img
               src="images/5296499_fb_facebook_facebook logo_icon.png" class="icon mx-3" /></div>
 
-               <!-- modal -->
+             
+</div>
+      </div>
+    </div>
+    
+  </div>
 
-            <div class="modal" tabindex="-1" id="forgotPasswordModal">
+  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.3/dist/umd/popper.min.js"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
+    integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r"
+    crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js"
+    integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+"
+    crossorigin="anonymous"></script>
+  <script src="js/script.js"></script>
+    <!-- modal -->
+
+    <div class="modal" tabindex="-1" id="forgotPasswordModal">
                 <div class="modal-dialog">
                     <div class="modal-content">
                         <div class="modal-header">
@@ -103,21 +121,6 @@ if (isset($_COOKIE["password"])) {
             </div>
 
             <!-- modal -->
-</div>
-      </div>
-    </div>
-  </div>
-
-  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.3/dist/umd/popper.min.js"></script>
-  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
-    integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r"
-    crossorigin="anonymous"></script>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js"
-    integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+"
-    crossorigin="anonymous"></script>
-  <script src="js/script.js"></script>
 </body>
 
 </html>
