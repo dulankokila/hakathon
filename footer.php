d1317d1a72d7cb5dd35921f0d3c6893ab617a9b2
KasunaSanthush <kgamindasanthus75@gmail.com>
2023-10-05 21:59:47 +0530

    footer

---
+++ footer.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" />
+
+    <link rel="stylesheet" href="bootstrap.css" />
+    <link rel="icon" href="images/black.jpg" />
+    <link rel="stylesheet" href="css/style.css">
+
+</head>
+
+<body>
+
+    <footer class="footerbg container-fluid ">
+
+        <div class="row bg-light bg-opacity-10">
+
+            <div class="col-12 text-md-start text ">
+
+                <div class="row d-flex align-items-center border-1 border-secondary border-bottom footerbox">
+                    <label class="col-12 col-lg-8 ps-lg-2 fs-5">
+                        Get connected with us on social networks :
+                    </label>
+
+                    <div class="col-12 col-lg-4 d-flex justify-content-lg-end justify-content-center pe-lg-5 gap-2">
+
+                        <a href="#" class="form-floating text-white list-inline-item">
+                            <i class="bi bi-facebook footerfornt"></i>
+                        </a>
+
+                        <a href="#" class="form-floating text-white list-inline-item">
+                            <i class="bi bi-twitter footerfornt"></i>
+                        </a>
+
+                        <a href="#" class="form-floating text-white list-inline-item">
+                            <i class="bi bi-whatsapp footerfornt"></i>
+                        </a>
+
+                        <a href="#" class="form-floating text-white list-inline-item">
+                            <i class="bi bi-linkedin footerfornt"></i>
+                        </a>
+
+                        <a href="#" class="form-floating text-white list-inline-item">
+                            <i class="bi bi-youtube footerfornt"></i>
+                        </a>
+
+                    </div>
+                </div>
+
+
+                <div class="row text-sm-center text-md-start">
+
+                    <div class="  col-12 col-md-4 col-lg-4 mt-3">
+                        <h5 class="text-uppercase mb-4 footerft">SALFORD </h5>
+                        <p>Here we are the salford.com&trade; to support you for &nbsp; accomplish your dessire by selling high
+                            quality products.</p>
+
+                    </div>
+
+                    <div class="col-12 col-md-4 col-lg-4 mt-3 ">
+                        <h5 class="text-uppercase mb-4 footerft">Contact</h5>
+                        <p><i class="bi bi-at"></i> salford@gmail.com</p>
+                        <p><i class="bi bi-telephone"></i> +94 70 588 5335</p>
+                        <p><i class="bi bi-printer"></i> +94 112 635 563</p>
+                    </div>
+                    <div class="col-12 col-md-4 col-lg-4 mt-3 ">
+                        <h5 class="text-uppercase mb-4 footerft">Location</h5>
+
+                        <p><i class="bi bi-geo-alt"></i> Maradana, Colombo 10, Sri Lanka</p>
+                    </div>
+
+                </div>
+
+                <div class="row border-1 border-bottom border-top border-secondary footerbox">
+                    <div class="col-12 d-flex justify-content-center align-items-center">
+                        <label class="fs-5">Copyright &copy; 2023 <a class="text-reset text-decoration-none fs-5" href="">salford.com</a> All Rights Reserved.</label>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+    </footer>
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.3/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
+    <script src="js/script.js"></script>
+
+</body>
+
+</html>
